12b8e5abf3a78240a101c243c990d38fa0303b42
Srinivas Pithani <srini.ondroid@gmail.com>
2016-12-26 23:25:37 -0500

    27.12 Added 2 files to reporsitory related to perl

---
+++ perl/strings.pl
@@ -0,0 +1,14 @@
+!#/usr/bin/perl
+
+print "hello \n";
+
+print 'hello' . " " . "world \n";
+
+print "Srini \n";
+
+print "The answer is " . 8 * 7 . "\n";
+print "The answer is ", 8 * 7 ,"\n";
+
+$fred = "Srinivas";
+print "$fred";
+print $fred;
