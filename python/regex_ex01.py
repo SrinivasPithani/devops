4d3b4ff794a04febab9f1dea4b48787af1e6832a
Srinivas Pithani <srini.ondroid@gmail.com>
2018-10-25 11:23:02 +0100

    25.10.2018 Added sysarg example

--- python/regex_ex01.py
+++ python/regex_ex01.py
@@ -1,8 +1,12 @@
 import re
 match = "ok we have a meeting at 4:00 & I need to send email to srini_ondroid@gmail.com \
-         ,along with this I need to send email to idt@id.com idt123@i123.com"
+         ,along with this I need to send email to Idt@id.com idt123@i123.com"
 mo = re.findall(r'\d:\d\d', match)
 mail_mo = re.findall(r'[\w+-_]+@[\w+.]+', match)
+
+stx = "This is not that"
+mostx = re.findall(r'this', stx,re.IGNORECASE)
+print (mostx)
 
 #Print the number of matches
 print("The number of email matches are " + str(len(mail_mo)) )
@@ -14,7 +18,7 @@ for items in range(len(mail_mo)):
 
 fo = open('testfile.txt', 'r')
 
-mo_mail = re.findall(r'[\w+-_]+@[\w+.]+', fo.read())
+#mo_mail = re.findall(r'[\w+-_]+@[\w+.]+', fo.read())
 
-for items in range(len(mo_mail)):
-      print(mo_mail[items].split("\n"))
+#for items in range(len(mo_mail)):
+#      print(mo_mail[items].split("\n"))
