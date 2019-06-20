cf8115332b40fb5698d3286e7d14a3e8598dc4fc
Srinivas Pithani <srini.ondroid@gmail.com>
2019-06-20 09:53:27 -0400

    24.06.2019 Modifiled the readlines funciton

--- ruby/file_analyzer.rb
+++ ruby/file_analyzer.rb
@@ -9,4 +9,8 @@ File.open("data/book.txt").each do |line|
 	t1 << line
 end
 puts "Number of lines using open method : #{line_count}"
-puts t1.length
+puts "Number of characters #{t1.length}"
+puts "Number of characters without spaces #{t1.gsub(/s+/, '').length}"
+
+puts "Number of words in this document using scan : #{t1.scan(/\w+/).length}"
+puts "Number of words in this document using split: #{t1.split.length} "
